2578b13283871493307d5f6d5f07fc162f3e3a21
mastermind <denis.barkar@gmail.com>
2018-03-23 01:42:52 +0300

    Final report stage

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ import os
 
 def show_image(name, image):
     max_width = 1980
+
     cv2.namedWindow(name)
     cv2.moveWindow(name, show_image.x, show_image.y)
-    show_image.x += image.shape[1] + 3
+    show_image.x += show_image.width + 82
     if show_image.x > max_width:
         show_image.x = 0
-        show_image.y += image.shape[0] + 81
+        show_image.y += show_image.height + 81
     cv2.imshow(name, image)
 
     path = os.path.split(input_file)
@@ -25,6 +26,8 @@ def show_image(name, image):
 show_image.x = 0
 show_image.y = 200
 show_image.step = 1
+show_image.width = 240
+show_image.height = 240
 
 
 def timing(f):
@@ -146,7 +149,7 @@ def find_face_corners(contours):
 
 
 @timing
-def affine_transform(image, corners):
+def projective_transform(image, corners):
     width = 120
     height = 120
 
@@ -155,6 +158,97 @@ def affine_transform(image, corners):
 
     M = cv2.getPerspectiveTransform(points1, points2)
     return cv2.warpPerspective(image, M, (width, height))
+
+
+@timing
+def get_colors(image):
+    height, width, _ = image.shape
+    stride = width / 3
+    offset = (width / 3) / 2
+
+    centers = [[offset, offset],             [offset + stride, offset],              [offset + stride * 2, offset],
+               [offset, offset + stride],    [offset + stride, offset + stride],     [offset + stride * 2, offset + stride],
+               [offset, offset + stride * 2], [offset + stride, offset + stride * 2], [offset + stride * 2, offset + stride * 2]]
+
+    calibrated_colors = {
+        (0, 0, 255):      [(314, 360), (0, 12)],  # red
+        (0, 255, 0):      [[106, 181]],  # green
+        (0, 165, 255):    [[13, 25]],    # orange
+        (255, 0, 0):      [[208, 215]],  # blue
+        (255, 255, 255):  [[216, 230]],  # white
+        (0, 255, 255):    [[44, 66]]     # yellow
+    }
+
+    error_color = (255, 0, 255)
+    colors = [error_color] * 9
+    for i, c in enumerate(centers):
+        c = tuple(map(int, c))
+        roi = image[c[0]-4:c[0]+4, c[1]-4:c[1]+4]
+        pixel = cv2.mean(roi)
+        float_pixel = np.float32([[[x / 255.0 for x in pixel]]])
+        hsv = cv2.cvtColor(float_pixel, cv2.COLOR_BGR2HSV)
+        h = hsv[0, 0, 0]
+        print('h:    ', h)
+        detected = error_color
+        for k, v in calibrated_colors.items():
+            found = False
+            for range in v:
+                if range[0] <= h <= range[1]:
+                    found = True
+            if found:
+                detected = k
+                break
+        colors[i] = detected
+
+    return colors
+
+
+def get_colors_image(colors):
+    width, height, = (120, 120)
+    stride = width // 3
+
+    image = np.ones((width, height, 3), np.uint8) * 255
+
+    x = 0
+    y = 0
+    for i, c in enumerate(colors):
+        cv2.rectangle(image, (x, y), (x + stride, y + stride), c, -1)
+        cv2.rectangle(image, (x, y), (x + stride, y + stride), (0, 0, 0), 2)
+        y += stride
+        if i == 2 or i == 5:
+            y = 0
+            x += stride
+    return image
+
+
+def slic(image):
+    num_superpixels = 400
+    num_levels = 4
+    prior = 2
+    num_histogram_bins = 5
+    num_iterations = 4
+
+    height, width, channels = image.shape
+    seeds = cv2.ximgproc.createSuperpixelSEEDS(width, height, channels, num_superpixels, num_levels, prior, num_histogram_bins)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    seeds.iterate(hsv_image, num_iterations)
+
+    color_image = np.zeros((height, width, 3), np.uint8)
+    color_image[:] = (0, 0, 255)
+
+    labels = seeds.getLabels()
+    num_label_bits = 2
+    labels &= (1 << num_label_bits) - 1
+    labels *= 1 << (16 - num_label_bits)
+
+    mask = seeds.getLabelContourMask(False)
+
+    mask_inv = cv2.bitwise_not(mask)
+    result_bg = cv2.bitwise_and(image, image, mask=mask_inv)
+    result_fg = cv2.bitwise_and(color_image, color_image, mask=mask)
+    result = cv2.add(result_bg, result_fg)
+
+    return result
 
 
 def main():
@@ -215,8 +309,15 @@ def main():
         cv2.circle(corners_frame, c, 5, (0, 255, 0), 3)
     show_image('Face corners', corners_frame)
 
-    face = affine_transform(frame, corners)
+    face = projective_transform(blurred_frame, corners)
     show_image('Face', face)
+
+    # slic_face = slic(face)
+    # show_image('SLIC', slic_face)
+
+    colors = get_colors(face)
+    colors_image = get_colors_image(colors)
+    show_image('Colors', colors_image)
 
 
 if __name__ == "__main__":
